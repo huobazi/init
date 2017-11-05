398b41cf253f30058eb0228b3852389a19cddd3a
Marble Wu <huobazi@gmail.com>
2017-11-05 10:33:56 +0800

    add install golang

---
+++ install_golang.sh
@@ -0,0 +1,7 @@
+GOREL=go1.7.3.linux-amd64.tar.gz
+wget  https://storage.googleapis.com/golang/$GOREL
+tar xfz $GOREL
+mv go /usr/local/go
+rm -f $GOREL
+PATH=$PATH:/usr/local/go/bin
+echo 'PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
